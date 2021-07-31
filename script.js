9a729fdea3ecc749d4b00cd20e21a15aa3595efc
Adyl <inamjanov.a@gmail.com>
2021-07-31 15:59:09 +0600

    zerocode

---
+++ script.js
@@ -0,0 +1,60 @@
+// const items = Array.from(document.querySelectorAll("li"))
+// let newArray = items.map((item) => item.textContent)
+//
+// console.log(newArray)
+
+
+// const task1 = () => {
+//     return document.querySelector("a").textContent
+// }
+// console.log(task1())
+
+
+// const task2 = () => {
+//     return Array.from(document.querySelectorAll("a"))
+//         .map((item) => item.textContent)
+// }
+// console.log(task2())
+
+
+// const task3 = () => {
+//     return Array.from(document.querySelectorAll(".nav__link__item"))
+//         .map((item) => item.textContent)
+// }
+// console.log(task3())
+
+
+// const task4 = () => {
+//     return Array.from(document.querySelectorAll(".nav__link__item")).map((item) => item.getAttribute("class"))
+// }
+// console.log(task4())
+
+
+// const task5 = () => {
+//     return Array.from(document.querySelectorAll(".nav__link"))
+//         .map((item) => item.getAttribute("data-link"))
+// }
+// console.log(task5())
+
+// const task7 = () => {
+//     return Array.from(document.querySelectorAll(".nav__link"))
+//         .forEach((item) => {
+//             item.textContent = `${item.getAttribute(`data-link`)} ${item.getAttribute(`class`).split(" ")[1]}`
+//         })
+// }
+// console.log(task7())
+
+
+// const task8 = () => {
+//     return Array.from(document.querySelectorAll('.nav__link'))
+//         .forEach(item => {
+//             if (item.getAttribute('class').split(' ')[1] === item.textContent){
+//                 return item
+//             }  return item.remove()
+//         })
+// }
+// console.log(task8())
+
+
+
+
